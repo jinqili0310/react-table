e8ef50d0bf71853dc1d0e5e852b740791ee14ed0
jinqili0310 <jinqi.li.310@gmail.com>
2024-08-30 17:49:24 -0700

    https://youtu.be/hson9BXU9F8?si=k2UzRgUyLGpBIfYs&t=746

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -4,10 +4,33 @@ import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./columns";
 
 export const BasicTable = () => {
-  useTable({
-    columns: COLUMNS,
-    data: MOCK_DATA,
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable({
+    columns,
+    data,
   });
 
-  return <div></div>;
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>column.render('Header')</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
